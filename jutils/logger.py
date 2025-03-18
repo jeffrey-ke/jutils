446060977796b421ad4b6577e7558c738762b1c0
Jeffrey Ke <jeffk@tesu.pc.cs.cmu.edu>
2025-03-18 10:45:03 -0400

    added a way to get a per metric count/step

--- jutils/logger.py
+++ jutils/logger.py
@@ -1,11 +1,16 @@
 from torch.utils.tensorboard import SummaryWriter
-
 # Singleton writer instance
 _writer_instance = None
+_iter_dict = {}
+def get_id_cur_step(id):
+    _iter_dict[id] = _iter_dict.setdefault(id, 0) + 1
+    return _iter_dict[id]
 
-def get_writer(log_dir="runs"):
+def get_writer(log_dir="runs", with_id=None):
     """Get a singleton instance of the TensorBoard SummaryWriter."""
     global _writer_instance
+    if id is not None:
+        _iter_dict[id] = 0
     if _writer_instance is None:
         _writer_instance = SummaryWriter(log_dir)
     return _writer_instance
