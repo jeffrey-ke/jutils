dbdcf83c74fa0b33ca078dce4bb0aa11f9ef882e
Jeffrey Ke <jeffk@tesu.pc.cs.cmu.edu>
2025-03-28 15:43:29 -0400

    minor spelling

--- jutils/nn.py
+++ jutils/nn.py
@@ -1,4 +1,4 @@
-import torch.nn
+import torch.nn as nn
 
 
 """
@@ -10,4 +10,24 @@ Needs to know the input shape (really? I guess I do because I need to know
 what the final dense layer looks like)
 def VAEEncoder(nn.Module):
     def __init__(self, ): #todo
+"""
+"""
+def ConvTDouble(nn.Module):
+    def __init__(self, 3d or 2d, in channels, out channels, relu_after=False, batchnorm_after=False):
+        layers = []
+        if 3d:
+            layer append ConvTranspose3d(out, in)
+        elif 2d:
+            layer append  ConvTranspose2d(out, in)
+        
+        if batch_norm after:
+            layer append batchnorm with out channels
+        if relu after:
+            layer append relu
 
+        self.me =  Sequential(*layers)
+
+    def forward(self, X):
+        return self.me(X)
+
+"""
