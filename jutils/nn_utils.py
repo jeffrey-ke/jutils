fc2a16dd807fb6877b1dd165d71ba12740c20968
Jeffrey <jeffk613@gmail.com>
2025-04-20 14:11:35 -0400

    Added the loraify function from PEFT, and added the print_trainable_params function. Also made a load_state_dict_up_to_classif head function which filters the state dict for the class_embed layer, removes it, and loads the filtered state dict into the passed detr model. I should, and can, make this more general.

--- jutils/nn_utils.py
+++ jutils/nn_utils.py
@@ -1,5 +1,7 @@
 import torch
+from peft import LoraConfig, get_peft_model, TaskType
 import torch.nn as nn
+from jutils.jnn import LoRA
 from jutils.config_classes import *
 # discovering that each network is pretty bespoke. I might be able to create a module
 # that automatically handles resconnections or skips based on the module's size...
@@ -33,8 +35,45 @@ def batchify(single_tensor):
 def unbatchify(single_batched_tensor):
     return single_batched_tensor.squeeze(0)
 
-def loraify(model, include=["attn", "linear"], r=4):
-    for name, module in model.modules():
+def loraify(model):
+    lora_cfg = LoraConfig(
+        r               = 8,
+        lora_alpha      = 32,
+        lora_dropout    = 0.05,
+        bias            = "none",
+        task_type       = TaskType.FEATURE_EXTRACTION,   
+        target_modules  = [
+            "q_proj", "k_proj", "v_proj", "out_proj",    # attention
+            "linear1", "linear2",                       # FFN
+        ],
+    )
+    detr_lora = get_peft_model(model, lora_cfg)     
+    return detr_lora
+
+def get_state_dict(path):
+    # args.frozen state_dict is the path to the checkpoint
+    checkpoint = torch.hub.load_state_dict_from_url(path, map_location='cpu')
+    return checkpoint['model']
+
+def print_trainable_parameters(model):
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param:.2f}"
+    )
+def load_state_dict_up_to_classif_head(detr, args):
+    state_dict = get_state_dict(path=args.resume)
+    for k,v in list(state_dict.items()):
+        if "class_embed" in k:
+            state_dict.pop(k)
+    detr.load_state_dict(state_dict, strict=False)
+
+def loraify_deprecated(model, include=["attn", "linear"], r=4):
+    for name, module in list(model.named_modules()):
         if isinstance(module, nn.Linear) or any(inc in name for inc in include):
             def get_immediate_parent(parent_obj, parent_arr):
                 parent = parent_obj
@@ -44,3 +83,4 @@ def loraify(model, include=["attn", "linear"], r=4):
             *parent_arr, child = name.split(".")
             immediate_parent = get_immediate_parent(model, parent_arr)
             setattr(immediate_parent, child, LoRA(module, r=r))
+            #TODO: some logging utility would be nice: like logger.log(immediate_parent, child)
